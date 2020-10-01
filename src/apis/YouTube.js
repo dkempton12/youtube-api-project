bb8988204290e02bab48553d29f80b6be811e75c
Dan Kempton <kemptondan@gmail.com>
2020-09-30 21:40:42 -0400

    Building out the youtube axios instance

--- src/apis/YouTube.js
+++ src/apis/YouTube.js
@@ -2,3 +2,13 @@ import React from "react";
 import axios from "axios";
 
 const KEY = "AIzaSyDMirQRdOz6yvVKKHukhFNFlydrEetiTyU";
+
+export default axios.create({
+  baseURL: "https://www.googleapis.com/youtube/v3",
+  params: {
+    part: "snippet",
+    type: "video",
+    maxResults: 5,
+    key: KEY,
+  },
+});
