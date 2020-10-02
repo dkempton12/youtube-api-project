ac5ff34c6c2cb63bd56e07f7509df704a5b149a7
Dan Kempton <kemptondan@gmail.com>
2020-10-01 21:43:40 -0400

    [debugging] Keep getting 403 after making get request

--- src/apis/YouTube.js
+++ src/apis/YouTube.js
@@ -1,10 +1,9 @@
-import React from "react";
 import axios from "axios";
 
 const KEY = "AIzaSyDMirQRdOz6yvVKKHukhFNFlydrEetiTyU";
 
 export default axios.create({
-  baseURL: "https://www.googleapis.com/youtube/v3",
+  baseURL: "https://www.googleapis.com/youtube/v3/",
   params: {
     part: "snippet",
     type: "video",
