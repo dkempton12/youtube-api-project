afa8fb8ed0fe2b9d1f33895af17522ef4b24a657
Dan Kempton <kemptondan@gmail.com>
2020-10-01 21:15:28 -0400

    Initializing state in App

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,9 @@ import React from "react";
 import SearchBar from "./SearchBar";
 
 class App extends React.Component {
+  state = {
+    videos: [],
+  };
   render() {
     return (
       <div className="ui container">
