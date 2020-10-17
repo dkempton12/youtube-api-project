6b48862793defafb51186a320b98ab679d8b867c
Dan Kempton <kemptondan@gmail.com>
2020-10-17 15:34:57 -0400

    Adding stylesheet for VideoItem component

--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const VideoItem = ({video}) => {
   return (
-    <div className="item">
+    <div className="item video-item">
       <img className="ui image" 
       src={video.snippet.thumbnails.medium.url} 
       alt={video.snippet.title}/>
