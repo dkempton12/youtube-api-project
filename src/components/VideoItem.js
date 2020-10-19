f7e9a6d16bc163e4f8b01e6f16fc7ed45959523d
Dan Kempton <kemptondan@gmail.com>
2020-10-19 18:19:26 -0400

    Updating VideoItem src attribute

--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -5,7 +5,7 @@ const VideoItem = ({video}) => {
   return (
     <div className="item video-item">
       <img className="ui image" 
-      src={video.snippet.thumbnails.medium.url} 
+      src={video.snippet.thumbnails.high.url} 
       alt={video.snippet.title}/>
       <div className='content'>
         <div className='header'>{video.snippet.title}</div>
