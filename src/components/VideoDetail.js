f6f4c93cd465445cdc4894a791ebbe72114b4ba1
Dan Kempton <kemptondan@gmail.com>
2020-10-20 18:17:59 -0400

    Adding embedded video using iframe to VideoDetail

--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -4,7 +4,12 @@ const VideoDetail = ({video}) => {
   if (!video) {
     return <div>Loading Video...</div>;
   }
+  // grabbing each selected video's URL
+  const videoURL = `https://www.youtube.com/embed/${video.id.videoId}`;
 return (
+  <div className="ui embed">
+    <iframe src="videoURL" title="Video Player"/>
+  </div>
   <div className="ui segment">
     <h4 className="ui header">{video.snippet.title}</h4>
     <p>{video.snippet.description}</p>
