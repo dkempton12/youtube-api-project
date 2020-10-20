91119f7199128e56dee59aa11da1eaa761e91f3f
Dan Kempton <kemptondan@gmail.com>
2020-10-20 18:19:32 -0400

    Adding a wrapper around VideoDetail JSX

--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -7,12 +7,14 @@ const VideoDetail = ({video}) => {
   // grabbing each selected video's URL
   const videoURL = `https://www.youtube.com/embed/${video.id.videoId}`;
 return (
+  <div>
   <div className="ui embed">
     <iframe src="videoURL" title="Video Player"/>
   </div>
   <div className="ui segment">
     <h4 className="ui header">{video.snippet.title}</h4>
     <p>{video.snippet.description}</p>
+  </div>
   </div>
 );
 };
