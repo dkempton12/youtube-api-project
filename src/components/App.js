4f5a73b8d87d7a5c70596996660cc75bbf3c9b99
Dan Kempton <kemptondan@gmail.com>
2020-10-27 18:45:14 -0400

    Testing grid layout

--- src/components/App.js
+++ src/components/App.js
@@ -36,10 +36,10 @@ class App extends React.Component {
         <SearchBar onFormSubmit={this.onTermSubmit} />
         <div className="ui grid">
           <div className="ui row">
-            <div className="nine wide column">
+            <div className="eleven wide column">
               <VideoDetail video={this.state.selectedVideo} />
             </div>
-            <div className="seven wide column">
+            <div className="five wide column">
               <VideoList
                 videos={this.state.videos}
                 onVideoSelect={this.onVideoSelect}
