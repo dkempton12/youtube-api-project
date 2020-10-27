3a4ce954259c93fb4e842b3c58e0f389058ea9ab
Dan Kempton <kemptondan@gmail.com>
2020-10-27 18:35:25 -0400

    Updating VideoItem styling

--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -1,22 +1,22 @@
 import React from 'react';
 
-const VideoDetail = ({video}) => {
+const VideoDetail = ({ video }) => {
   if (!video) {
     return <div>Loading Video...</div>;
   }
   // grabbing each selected video's URL
   const videoURL = `https://www.youtube.com/embed/${video.id.videoId}`;
-return (
-  <div>
-  <div className="ui embed">
-    <iframe src={videoURL} title="Video Player"/>
-  </div>
-  <div className="ui segment">
-    <h4 className="ui header">{video.snippet.title}</h4>
-    <p>{video.snippet.description}</p>
-  </div>
-  </div>
-);
+  return (
+    <div>
+      <div className="ui embed">
+        <iframe src={videoURL} title="Video Player" />
+      </div>
+      <div className="ui segment">
+        <h4 className="ui header">{video.snippet.title}</h4>
+        <p>{video.snippet.description}</p>
+      </div>
+    </div>
+  );
 };
 
 export default VideoDetail;
